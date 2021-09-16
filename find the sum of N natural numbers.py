04ef4dac49947fe110fd0962862d26eaf86c8788
mani kumar battula <87745578+mani3-debug@users.noreply.github.com>
2021-09-16 14:58:35 +0530

    Add files via upload

---
+++ find the sum of N natural numbers.py
@@ -0,0 +1,18 @@
+#num = int(input("enter the number:"))
+#sum = num *(num+1) /2
+#print(" the sum of N natural number is {}".format(sum))
+
+
+#num= int(input(" enter the number:"))
+#value=0
+#for i in range(1,num+1):
+   #value= value+1
+#print("the sum of N numbers",value)
+
+
+num=int(input("insert a number:"))
+if num > 0:
+ print("the number is positive")
+
+else:
+ print("the number is negative")
