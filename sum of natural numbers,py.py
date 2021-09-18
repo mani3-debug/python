3af659c97b204ea6a3bb8632572f6ad6f109b970
mani kumar battula <87745578+mani3-debug@users.noreply.github.com>
2021-09-18 17:13:45 +0530

    Add files via upload

---
+++ sum of natural numbers,py.py
@@ -0,0 +1,25 @@
+num = int(input("Enter a number: "))  
+  
+if num > 0:  
+   print("Enter a positive number")  
+else:  
+   sum = 0  
+   # use while loop to iterate un till zero  
+   while(num > 0):  
+       sum += num  
+       num -= 1  
+   print("The sum is",sum)
+
+
+
+   
+
+
+num= int(input(" enter the number:"))
+value=0
+for i in range(1,num+1):
+   value= value+1
+print("the sum of N numbers",value)
+
+
+   
